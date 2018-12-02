caf4799709b9f093f8b46d46d618f36a527a8e9c
rururu16 <ynk618172@gmail.com>
2018-12-03 00:23:04 +0900

    mod

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -12,7 +12,6 @@ class LinebotController < ApplicationController
     uri = URI.parse("https://www.healthplanet.jp/status/innerscan.json?access_token=#{ENV["TANITA_TOKEN"]}&date=1&tag=6021")
     json = Net::HTTP.get(uri)
     result = JSON.parse(json)
-    binding.pry
     @data = result['data']
     @last = @data[-1]
     @last2 = @data[-2]
